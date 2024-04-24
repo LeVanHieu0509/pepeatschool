f4c7997ca141ed590138ae5519293db398da8bb5
bladeonfire2402 <liemp966@gmail.com>
2024-04-24 22:52:26 +0700

    comment gì trong ngoặc này cũng được

--- components/profile-author.tsx
+++ components/profile-author.tsx
@@ -1,7 +1,308 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 
-export const ProfileAuthor = () => {
+
+export const ProfileAuthor1=()=>{
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/cDBvvx8/Nguy-n-Minh-An.jpg"
+            alt="Nguyễn Minh An"
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Nguyễn Minh An
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Chuyên viên phân tích thị trường
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+export const ProfileAuthor2 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/Ny4pR05/Anh-giang-vien-dai-hoc-8-1180x760.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Alex Dương
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Chuyên viên phân tích thị trường
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor3 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/rHCSwM0/L-Nguy-n-Anh-T-m.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Lê Nguyễn Anh Tâm
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Thành viên cộng đồng Pepe at school
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor4=()=>{
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/Bwg8x3T/Tr-ng-ng.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Trường Đặng 
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Nhà đầu tư
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+export const ProfileAuthor5 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/CzYGWYf/Qu-Tr-n-Hu-nh.jpg"
+            alt="Quế Trân Huỳnh"
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Quế Trân Huỳnh
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Sinh viên đại học
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor6 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/Qbqb7FX/Ng-Minh-Giang.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Ngô Minh Giang
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Giảng Viên
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor7=()=>{
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/qWpxx7c/Ryan-Nguy-n.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Ryan Nguyễn
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Nhà đầu tư
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+export const ProfileAuthor8 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/vmRyrC2/Long-Phan.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Long Phan
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Thành viên cộng đồng Pepe at school
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor9 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/JCszY5s/David-Tr-n.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              David Trần
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Thành viên cộng đồng Pepe at school
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor10=()=>{
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/C0JFKNh/ng-Thi-n-Trang.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Đặng Thiên Trang
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Nhà đầu tư
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+export const ProfileAuthor11 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/mR1M5XR/Nguy-n-Phan-Ho-ng-Khoa.jpg"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Nguyễn Phan Hoàng Khoa
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Nhiếp ảnh gia/ Nhà đầu tư
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor12=()=>{
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://images.unsplash.com/photo-1632910121591-29e2484c0259?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxjb2RlcnxlbnwwfDB8fHwxNzEwMTY0NjIzfDA&ixlib=rb-4.0.3&q=80&w=1080"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Nguyễn Tấn Hiệp
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Chuyên gia phân tích thị 
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+export const ProfileAuthor13 = () => {
   return (
     <div className="text-sm leading-6">
       <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
@@ -19,6 +320,83 @@ export const ProfileAuthor = () => {
             </div>
             <div className="mt-0.5 dark:text-slate-300">
               Chuyên gia phân tích thị trường
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthor14 = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://images.unsplash.com/photo-1632910121591-29e2484c0259?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxjb2RlcnxlbnwwfDB8fHwxNzEwMTY0NjIzfDA&ixlib=rb-4.0.3&q=80&w=1080"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              Nguyễn Tấn Hiệp
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Chuyên gia phân tích thị trường
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+export const ProfileAuthorGrayScale=()=>{
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://images.unsplash.com/photo-1632910121591-29e2484c0259?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxjb2RlcnxlbnwwfDB8fHwxNzEwMTY0NjIzfDA&ixlib=rb-4.0.3&q=80&w=1080"
+            alt=""
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              GrayScale
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Quỹ đầu tư hàng đầu
+            </div>
+          </div>
+        </figcaption>
+      </figure>
+    </div>
+  );
+}
+export const ProfileAuthorBlackRock = () => {
+  return (
+    <div className="text-sm leading-6">
+      <figure className="w-50 relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
+        <figcaption className="flex items-center space-x-4">
+          <img
+            src="https://i.ibb.co/997yh9K/th-3.jpg"
+            alt=""
+            width={60}
+            height={60}
+            className="flex-none w-14 h-14 rounded-full object-cover"
+            loading="lazy"
+            decoding="async"
+          />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              BlackRock
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">
+              Quỹ đầu tư hàng đầu 
             </div>
           </div>
         </figcaption>
