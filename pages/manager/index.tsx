30d916715ed238376d8e3e66a1b561ff82119cee
LeVanHieu0509 <levanhieu.huflit@gmail.com>
2024-05-19 12:04:35 +0700

    fix build

--- pages/manager/index.tsx
+++ pages/manager/index.tsx
@@ -1,11 +1,10 @@
 import AdminScreen from "src/screens/admin";
-import { AdminCourseWrapper } from "./styled";
 
 interface AdminCourseProps {}
 
 const AdminCourse = ({}: AdminCourseProps) => {
   return (
-    <AdminCourseWrapper>
+    <>
       <h1>Create New NFT</h1>
       <p>
         You can set preferred display name, create your profile URL and manage
@@ -13,7 +12,7 @@ const AdminCourse = ({}: AdminCourseProps) => {
       </p>
 
       <AdminScreen />
-    </AdminCourseWrapper>
+    </>
   );
 };
 
